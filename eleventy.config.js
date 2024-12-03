141ac058b424a8859c2bbae22c3b55185fb10448
Temps91 <jorgebd3045@gmail.com>
2024-12-03 00:08:20 -0600

    Update eleventy.config.js

--- eleventy.config.js
+++ eleventy.config.js
@@ -1,16 +1,15 @@
 module.exports = function(eleventyConfig) {
-    // Crear una colección que recoja todos los archivos .md
     eleventyConfig.addCollection("searchIndex", function(collectionApi) {
         return collectionApi.getAll().filter(item => item.inputPath.endsWith(".md")).map(item => {
             return {
                 title: item.data.title || item.fileSlug,
                 url: item.url,
-                excerpt: item.templateContent.substring(0, 150)  // Tomar los primeros 150 caracteres como fragmento
+                excerpt: item.templateContent.substring(0, 150)
             };
         });
     });
 
-    // Pasar el índice a un archivo JSON
+
     eleventyConfig.addPassthroughCopy("search.json");
 
     return {
